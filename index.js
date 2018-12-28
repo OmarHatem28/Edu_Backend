f64a9bc90e65ae939f0c662b1ea04f7130ecaa4f
Omar <omarh.ismail1@gmail.com>
2018-12-29 00:33:42 +0200

    Integrated Backend With Frontend, added 2 routes: home & courses

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const courses = require('./routes/courses');
+const home = require('./routes/home');
 const express = require('express');
 const app = express();
 
 
 app.use(express.json());
 
-
+app.use('/',home);
 app.use('/courses',courses);
 
 
-// app.use(express.static(__dirname + '/frontend'));
-// app.set('frontend', __dirname + '/frontend');
-// app.engine('html', require('ejs').renderFile);
-// app.set('view engine', 'html');
+app.set('views', __dirname + '/frontend');
+app.use(express.static('frontend'));
+app.engine('.html', require('ejs').renderFile);
+
+
+
 
 app.listen('3000',() => {
     console.log('listening on port 3000...');
